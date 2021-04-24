94bdc896bb3414858dc75512dc85bfbe8b54b462
d3dx13 <36724128+d3dx13@users.noreply.github.com>
2021-04-24 12:42:43 +0300

    Update a_2_1_prepare_OpenCV.sh

--- install/base/a_2_1_prepare_OpenCV.sh
+++ install/base/a_2_1_prepare_OpenCV.sh
@@ -42,4 +42,14 @@ sudo apt-get install -y qtbase5-dev
 sudo apt-get install -y qtdeclarative5-dev
 sudo apt-get install -y libopenblas-dev
 sudo apt-get install -y liblapacke-dev
+sudo apt-get update
+sudo apt-get install -y build-essential make cmake cmake-curses-gui git g++ pkg-config curl
+sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libeigen3-dev libglew-dev libgtk2.0-dev
+sudo apt-get install -y libtbb2 libtbb-dev libv4l-dev v4l-utils qv4l2 v4l2ucp
+sudo apt-get install -y libdc1394-22-dev libxine2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt-get install -y libjasper-dev
+sudo apt-get install -y libjpeg8-dev libjpeg-turbo8-dev libtiff-dev libpng-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
+sudo apt-get install -y libatlas-base-dev libopenblas-dev liblapack-dev liblapacke-dev gfortran
+sudo apt-get install -y qt5-default
 
