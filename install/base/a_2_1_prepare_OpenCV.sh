7801d8b69f4c353918268dbd3797884316013fa1
d3dx13 <zhidkov2846@gmail.com>
2021-04-26 11:28:18 +0300

    Адаптация кода под PC. OpenCV не завершён

--- install/base/a_2_1_prepare_OpenCV.sh
+++ install/base/a_2_1_prepare_OpenCV.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-
 sudo apt install -y build-essential cmake pkg-config unzip yasm git checkinstall
 sudo apt install -y libjpeg-dev libpng-dev libtiff-dev
 sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
@@ -13,9 +8,9 @@ sudo apt install -y libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev 
 sudo apt install -y libfaac-dev libmp3lame-dev libvorbis-dev
 sudo apt install -y libopencore-amrnb-dev libopencore-amrwb-dev
 sudo apt-get install -y libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils
-# cd /usr/include/linux
-# sudo ln -s -f ../libv4l1-videodev.h videodev.h
-# cd -
+cd /usr/include/linux
+sudo ln -s -f ../libv4l1-videodev.h videodev.h
+cd -
 sudo apt-get install -y libgtk-3-dev
 sudo apt-get install -y python3-dev python3-pip
 sudo apt-get install -y python-dev python-pip
@@ -42,7 +37,7 @@ sudo apt-get install -y qtbase5-dev
 sudo apt-get install -y qtdeclarative5-dev
 sudo apt-get install -y libopenblas-dev
 sudo apt-get install -y liblapacke-dev
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y build-essential make cmake cmake-curses-gui git g++ pkg-config curl
 sudo apt-get install -y libavcodec-dev libavformat-dev libavutil-dev libswscale-dev libeigen3-dev libglew-dev libgtk2.0-dev
 sudo apt-get install -y libtbb2 libtbb-dev libv4l-dev v4l-utils qv4l2 v4l2ucp
@@ -52,4 +47,9 @@ sudo apt-get install -y libjpeg8-dev libjpeg-turbo8-dev libtiff-dev libpng-dev
 sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev
 sudo apt-get install -y libatlas-base-dev libopenblas-dev liblapack-dev liblapacke-dev gfortran
 sudo apt-get install -y qt5-default
+sudo apt-get install -y libbz2-dev
 
+sudo /bin/bash -c 'echo export CUDA_HOME=/usr/local/cuda >> /etc/bash.bashrc'
+sudo /bin/bash -c 'echo export PATH=/usr/local/cuda/bin:$PATH >> /etc/bash.bashrc'
+sudo /bin/bash -c 'echo export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH >> /etc/bash.bashrc'
+sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
